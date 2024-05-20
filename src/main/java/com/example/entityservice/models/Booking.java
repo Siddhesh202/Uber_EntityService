8547f9aed6fb12307220a008d1bfdbe50b243093
Siddhesh Pisal <siddheshpisal76@gmail.com>
2024-05-21 00:53:13 +0530

    Added db models

---
+++ src/main/java/com/example/entityservice/models/Booking.java
@@ -0,0 +1,38 @@
+package com.example.entityservice.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.websocket.server.ServerEndpoint;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Driver driver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Passenger passenger;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+
+}
